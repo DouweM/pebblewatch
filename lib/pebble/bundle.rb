fe0e2fab59d85adfdf2bcfcf2c03bdc7a0918edf
Ps0ke <mail@ps0ke.de>
2013-09-28 20:55:37 +0200

    Make code comply to the code style guide lines.

--- lib/pebble/bundle.rb
+++ lib/pebble/bundle.rb
@@ -14,21 +14,21 @@ module Pebble
 		class AppInfo < BinData::Record
 			endian :big
 
-			string  :header, :length => 8, :asserted_value => "PBLAPP\0\0"
+			string  :header, length: 8, asserted_value: "PBLAPP\0\0"
 			version :struct_version
 			version :sdk_version
 			version :app_version
 			uint16  :bin_size
 			uint32  :bin_offset
 			uint32  :crc
-			string  :name, :length => 32, :trim_padding => true
-			string  :company, :length => 32, :trim_padding => true
+			string  :name, length: 32, trim_padding: true
+			string  :company, length: 32, trim_padding: true
 			uint32  :icon_resource_id
 			uint32  :sym_table_addr
 			uint32  :flags
 			uint32  :reloc_list_start
 			uint32  :num_reloc_entries
-			array   :uuid, :type => :uint8, :initial_length => 16
+			array   :uuid, type: :uint8, initial_length: 16
 		end
 	end
 
@@ -36,8 +36,8 @@ module Pebble
 		def initialize(path)
 			@path = path
 			@zip = Zip::File.open(path)
-			@manifest = JSON.parse(@zip.file.read('manifest.json'))
-			@bin = @zip.file.read('pebble-app.bin')
+			@manifest = JSON.parse(@zip.file.read("manifest.json"))
+			@bin = @zip.file.read("pebble-app.bin")
 			@header = Bin::AppInfo.read(@bin)
 		end
 
@@ -47,51 +47,40 @@ module Pebble
 		alias :app_metadata :header
 
 		def is_firmware?
-			@manifest.has_key? 'firmware'
+			@manifest.has_key?("firmware")
 		end
 
 		def is_app?
-			@manifest.has_key? 'application'
+			@manifest.has_key?("application")
 		end
 
 		def has_resources?
-			@manifest.has_key? 'resources'
+			@manifest.has_key?("resources")
 		end
 
 		def firmware_info
-			if self.is_firmware?
-				@manifest['firmware']
-			else
-				nil
-			end
+			@manifest["firmware"]
 		end
 
 		def app_info
-			if self.is_app?
-				@manifest['application']
-			else
-				nil
-			end
+			@manifest["application"]
 		end
 
 		def resources_info
-			if self.has_resources?
-				@manifest['resources']
-			else
-				nil
-			end
+			@manifest["resources"]
 		end
 
 		def uuid_hex
-			@header.uuid.map{|byte| byte.to_i.to_s(16)}
+			@header.uuid.map{ |byte| byte.to_i.to_s(16) }
 		end
 
 		def uuid_hex_string
-			@header.uuid.map{|byte| "%02X" % byte.to_i}.join(' ')
+			@header.uuid.map{ |byte| "%02X" % byte.to_i }.join(' ')
 		end
 
 		def close
 			@zip.close
+			@zip = nil
 		end
 	end
 end
